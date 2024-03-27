d9e162e068e9a8d253eff0766b39cc470d78574c
MateuszGajdowicz <155658500+MateuszGajdowicz@users.noreply.github.com>
2024-03-27 14:53:14 +0100

    jop

--- index.js
+++ index.js
@@ -2,3 +2,6 @@
         var NewPlantName1 = localStorage.getItem("NewPlantName1"); // Zmieniono na "NewPlantName1"
         if (NewPlantName1)
             document.getElementById("p1").textContent = NewPlantName1;
+        var NewPlantName2 = localStorage.getItem("NewPlantName2");
+        if (NewPlantName2)
+            document.getElementById("p1").textContent = NewPlantName2;}
