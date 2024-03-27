70acbe5a57367e545bc35c041508c86366b6d1ce
MateuszGajdowicz <155658500+MateuszGajdowicz@users.noreply.github.com>
2024-03-27 04:00:33 +0100

    myplants

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-        var NewPlantName = localStorage.getItem("NewPlantName");
+        var NewPlantName = localStorage.getItem("NewPlantName1");
         if (NewPlantName)
             document.getElementById("plant1label").textContent = NewPlantName;
         }
